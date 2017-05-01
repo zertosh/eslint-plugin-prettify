404a8b69f4e0601479b063313d895258aec7c27a
Andres Suarez <zertosh@gmail.com>
2017-05-01 00:31:06 -0400

    merge docblock into rule.js

--- rule.js
+++ rule.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const diff = require('fast-diff');
-const docblock = require('./docblock');
 let prettier;
 
 const fbPragma = 'format';
@@ -57,7 +56,7 @@ module.exports = {
       )) {
         return {};
       }
-      const parsed = docblock.parse(firstComment.value);
+      const parsed = parseDocblock(firstComment.value);
       if (!parsed.hasOwnProperty(pragma) || parsed[pragma] !== '') {
         return {};
       }
@@ -197,4 +196,38 @@ function getLocFromIndex(context, index) {
       offset = offset + lines[i].length + 1;
     }
   }
+}
+
+// This is just the parse as object function from:
+// https://github.com/facebook/react-native/blob/d72c6fd/packager/react-packager/src/node-haste/DependencyGraph/docblock.js
+
+const commentStartRe = /^\/\*\*/;
+const commentEndRe = /\*\/$/;
+const wsRe = /[\t ]+/g;
+const stringStartRe = /(\r?\n|^) *\*/g;
+const multilineRe = /(?:^|\r?\n) *(@[^\r\n]*?) *\r?\n *([^@\r\n\s][^@\r\n]+?) *\r?\n/g;
+const propertyRe = /(?:^|\r?\n) *@(\S+) *([^\r\n]*)/g;
+
+function parseDocblock(docblock_) {
+  let docblock = docblock_
+    .replace(commentStartRe, '')
+    .replace(commentEndRe, '')
+    .replace(wsRe, ' ')
+    .replace(stringStartRe, '$1');
+
+  // Normalize multi-line directives
+  let prev = '';
+  while (prev !== docblock) {
+    prev = docblock;
+    docblock = docblock.replace(multilineRe, '\n$1 $2\n');
+  }
+  docblock = docblock.trim();
+
+  const result = {};
+  let match;
+  while ((match = propertyRe.exec(docblock))) {
+    result[match[1]] = match[2];
+  }
+
+  return result;
 }
